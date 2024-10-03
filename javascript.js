10cb4696184d589d2fc7a451a4e0c481e2fe7f71
Shoxjaxon <sh-92@mail.ru>
2024-10-03 16:23:38 +0500

    first commit 00

---
+++ javascript.js
@@ -0,0 +1,76 @@
+"use strict";
+
+
+
+// const number = 100;
+// const borderColor = 'red';
+
+// const person = {
+//     name: 'Olma'
+// };
+// person.name = 'Behi';
+
+// const place = confirm('San shu yerdamisan')
+// console.log(place)
+
+
+// const currentPlace = prompt("Sen qayerdasan", "")
+// console.log(currentPlace)
+
+
+// const age = +prompt("Yosjhingiz", "19")
+// console.log(typeof(age + 10))
+
+// console.log(person)
+
+// const favouriteColor = [];
+// favouriteColor[0] = prompt('ranglar 1', '');
+// favouriteColor[1] = prompt('ranglar 2', '');
+// favouriteColor[2] = prompt('ranglar 3', '');
+
+// console.log(favouriteColor); 
+
+
+// const channel = prompt("havola");
+// console.log(`http://10.1.54.254:8080/notary/index, ${channel}`)
+
+// const user = "Shoxjaxon";
+// const job = "notarius yordamchisi";
+// console.log(`uning ismi ${user}, lavozimi ${job}`)
+
+// console.log(9 + "10")
+
+// 10 chi darsga keldik
+
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+// console.log(10 % 3)
+
+
+// console.log(5*5 === "25");
+
+
+// const isAge = true;
+// const isClose = true;
+// const isChaced = false;
+// console.log(isAge && isClose && !isChaced);
+
+
+// console.log(2 + 2 * 2 !== 6);
+
+
+// 11 darsga keldik
+
+// 12 darsga keldik
