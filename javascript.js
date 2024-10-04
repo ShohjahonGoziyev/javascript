faa69150b0102dad22213672519daa41d68fba43
Shoxjaxon <sh-92@mail.ru>
2024-10-04 15:30:13 +0500

    keyingi darsi

--- javascript.js
+++ javascript.js
@@ -74,3 +74,30 @@
 // 11 darsga keldik
 
 // 12 darsga keldik
+
+
+// const numberOfSeries = +prompt ("Nechra serial kordingiz?", "");
+// const seriesDB = {
+//     count: numberOfSeries,
+//     series: {},
+//     actors: {},
+//     genres: [],
+//     privat: false  
+
+// };
+
+// const a = prompt("Oxirgi korgan serialingiz?"),
+//     b = prompt("Nechi baxo berasiz?"),
+//     c = prompt("Oxirgi  korgan serialingiz?"),
+//     d = prompt("Nechi baxo berasiz?");
+
+// seriesDB.series[a] = b;
+// seriesDB.series[c] = d;
+// const theif = {
+//     jacket: "Black",
+// };
+// const property = "age",
+//     value = 18;
+// theif[property] = value;
+
+// console.log(theif)
